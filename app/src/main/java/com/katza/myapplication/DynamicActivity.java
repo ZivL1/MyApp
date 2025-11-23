c28e890b6edae27e39e75d954cb8ae7bf51054b8
ZivL1 <another122122@gmail.com>
2025-11-23 16:46:20 +0200

    not finished new layout + dynamic

---
+++ app/src/main/java/com/katza/myapplication/DynamicActivity.java
@@ -0,0 +1,35 @@
+package com.katza.myapplication;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    ImageView iv;
+    LinearLayout main;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dynamic);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        iv = new ImageView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100,100);
+        iv.setLayoutParams(layoutParams);
+
+        main =(LinearLayout)findViewById(R.id.llmain);
+
+    }
+}
